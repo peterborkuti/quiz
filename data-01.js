4d01b17076f2b20b10fef4ae6e0fbf426c1164e3
Peter Borkuti <peter.borkuti@liferay.com>
2014-01-04 19:41:02 +0100

    Create data-01.js

---
+++ data-01.js
@@ -0,0 +1,17 @@
+[
+ { multiple: 'Name the liferay plugin-types!',
+   answers: ['portlets','themes','layout templates','web modules','hooks','ext plugins','containers','services','pluggers','connectors'],
+   good: 6
+ },
+ { single: 'In which version introduced Liferay the idea of hot-deployable plugins?',
+   answers: ['4.3.0', '2.6.1', '5.2', '6.0', '3.2.0']
+ },
+ { single: 'From which version Ext plugins are not supported?',
+   answers: ['6.0', '4.3.0', '2.6.1', '5.2', '3.2.0']
+ },
+ { short: 'name the environment with which you can develeop hot-deployable plugins for Liferay!',
+   answers : [ 'plugin[s]{0,1}[ ]*sdk']
+ },
+ 
+ 
+]
