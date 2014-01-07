22c281c062e249b329252a51c7a3c2255b0466df
Peter Borkuti <peter.borkuti@liferay.com>
2014-01-07 17:48:02 +0100

    matches is displaying

--- html/js/quiz.js
+++ html/js/quiz.js
@@ -1,44 +1,59 @@
-function shuffle(array) {
-    var counter = array.length, temp, index;
+$(function() {
+console.log("DOM is ready");
 
-    // While there are elements in the array
-    while (counter--) {
-        // Pick a random index
-        index = (Math.random() * counter) | 0;
+	function Question(data, _index) {
+			this.index = _index;
+			if ( typeof data.matches !== 'undefined' ) {
+				this.questionType = 'matches';
+				this.matches = data.matches;
+				this.answers = data.answers;
+			} else {
+				console.log('unknown question type');
+			}
+			this.getShuffledAnswers = function() {
+			var array = this.answers;
+			var counter = array.length, temp, index;
 
-        // And swap the last element with it
-        temp = array[counter];
-        array[counter] = array[index];
-        array[index] = temp;
-    }
+			// While there are elements in the array
+			while (counter--) {
+				// Pick a random index
+				index = (Math.random() * counter) | 0;
 
-    return array;
-}
+				// And swap the last element with it
+				temp = array[counter];
+				array[counter] = array[index];
+				array[index] = temp;
+			}
 
-function Question(data, _index) {
-	this.index = _index;
-	if ( typeof data.matches !== 'undefined' ) {
-		this.questionType = 'matches';
-		this.matches = data.matches;
-		this.answers = data.answers;
-	} else {
-		console.log('unknown question type');
+			return array;
+		}
+
 	}
 
-}
+	function QuestionsViewModel() {
+		// Data
+		var self = this;
 
-function QuestionsViewModel() {
-    // Data
-    var self = this;
+		self.questions = ko.observableArray([]);
+		$.get( "data/data.json", function(allData) {
+			var data = $.parseJSON(allData);
+			console.log("data received from server");
+			var mappedQuestions = $.map(data, function(item, index) { return new Question(item, index) });
+			self.questions(mappedQuestions);
+		  console.log( "success" );
+		})
+		  .done(function() {
+			alert( "second success" );
+		  })
+		  .fail(function() {
+			alert( "error" );
+		  })
+		  .always(function() {
+			alert( "finished" );
+		  });
+	}
 
-    self.questions = [];
-    $.getJSON("../data-01.js", function(allData) {
-        self.questions = $.map(allData, function(item, index) { return new Question(item, index) });
-		//self.questions(mappedQuestions);
-    });
+	ko.applyBindings(new QuestionsViewModel());
 
-}
-
-$(function() {
-ko.applyBindings(new QuestionsViewModel());
+	console.log("KO ran");
 });
---
+++ server.js
@@ -0,0 +1,47 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var mimeTypes = {
+    "html": "text/html",
+    "jpeg": "image/jpeg",
+    "jpg": "image/jpeg",
+    "png": "image/png",
+    "js": "text/javascript",
+    "css": "text/css",
+	"json": "text/json" };
+
+http.createServer(function (request, response) {
+    console.log('request starting...');
+	
+	var filePath = '.' + request.url;
+	if (filePath == './')
+		filePath = './index.htm';
+		
+	var extname = path.extname(filePath);
+	var contentType = mimeTypes[extname];
+	if (contentType == "") {
+		contentType = 'text/html';
+	}
+
+	path.exists(filePath, function(exists) {
+	
+		if (exists) {
+			fs.readFile(filePath, function(error, content) {
+				if (error) {
+					response.writeHead(500);
+					response.end();
+				}
+				else {
+					response.writeHead(200, { 'Content-Type': contentType });
+					response.end(content, 'utf-8');
+				}
+			});
+		}
+		else {
+			response.writeHead(404);
+			response.end();
+		}
+	});
+	
+}).listen(8125);
+console.log('Server running at http://127.0.0.1:8125/');
